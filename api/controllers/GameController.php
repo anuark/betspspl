7559abc6aa47bba9e814a7385755b1e247375c28
Anuar Kilgore <jaicof@gmail.com>
2018-06-12 10:10:31 -0600

    Game controller CORS

--- api/controllers/GameController.php
+++ api/controllers/GameController.php
@@ -8,6 +8,23 @@ use api\models\Game;
 
 class GameController extends Controller
 {
+
+    public function behaviors()
+    {
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Allow-Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['*'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Max-Age' => ['3600']
+                ]
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return Game::find()->all();
