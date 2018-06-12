967f4d7b5e9dd74f2243e3e459f37878c8e7edff
Anuark <arknium@gmail.com>
2018-06-12 16:31:57 -0600

    auth_key field

--- api/models/User.php
+++ api/models/User.php
@@ -7,7 +7,7 @@ class User extends \common\models\User
     public function fields()
     {
         $fields = parent::fields();
-        unset($fields['password_hash'], $fields['password_reset_token'], $fields['auth_key']);
+        unset($fields['password_hash'], $fields['password_reset_token']);
         return $fields;
     }
 }
