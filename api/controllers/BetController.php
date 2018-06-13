b041c9d55a14cff7eb4fc953defec670c40fd3ab
Anuark <arknium@gmail.com>
2018-06-12 18:07:43 -0600

    bet create

--- api/controllers/BetController.php
+++ api/controllers/BetController.php
@@ -5,6 +5,7 @@ namespace api\controllers;
 use Yii;
 use yii\rest\ActiveController;
 use api\models\Bet;
+use yii\web\BadRequestHttpException;
 
 class BetController extends ActiveController
 {
@@ -26,5 +27,23 @@ class BetController extends ActiveController
     {
         $bets = Bet::find()->where(['user_id' => Yii::$app->user->id])->all();
         return $bets;
+    }
+
+    public function actionCreate()
+    {
+        $model = new Bet();
+        if ($model->load(Yii::$app->request->post(), '')) {
+            $bet = Bet::find()->where(['user_id' => $model->user_id, 'game_id' => $model->game_id])->one();
+            if (!$bet) {
+                $model->save();
+                return $model;
+            }
+
+            $bet->load(Yii::$app->request->post(), '');
+            $bet->update();
+            return $bet;
+        }
+
+        throw new BadRequestHttpException(getError($model));
     }
 }
