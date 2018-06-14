9323177d5e8aacdfe48eb97bff9e7634e1e7bab7
Anuar Kilgore <jaicof@gmail.com>
2018-06-14 10:37:35 -0600

    user points field

--- api/models/User.php
+++ api/models/User.php
@@ -8,6 +8,14 @@ class User extends \common\models\User
     {
         $fields = parent::fields();
         unset($fields['password_hash'], $fields['password_reset_token']);
+        $fields['points'] = function ($model) {
+            return (int) (new Query())
+                ->select('SUM(asserted)')
+                ->from('bet')
+                ->where(['user_id' => $model->id])
+                ->scalar();
+        };
+
         return $fields;
     }
 }
