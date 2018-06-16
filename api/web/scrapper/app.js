76680f1a762983291c9316eee21db8b7ff48e3e8
Anuar Kilgore <jaicof@gmail.com>
2018-06-16 12:30:14 -0600

    worked on scrapper

--- api/web/scrapper/app.js
+++ api/web/scrapper/app.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
+const fs = requier('fs');
 const http = require('http');
 
-const getHtml = (async () => {
+(async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto('https://www.whoscored.com/Regions/247/Tournaments/36/Seasons/5967/Stages/15737/Fixtures/International-FIFA-World-Cup-2018');
@@ -9,15 +10,22 @@ const getHtml = (async () => {
     let bodyHtml = await page.evaluate(() => document.body.innerHTML);
     await browser.close();
 
-    return bodyHtml;
+    fs.writeFile(`/tmp/${Date.now()}".html`, bodyHtml, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    })
+})().catch((reason) => {
+    console.log(reason);
 });
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    getHtml().then((v) => {
-        res.end(v);
-    });
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     getHtml().then((v) => {
+//         res.end(v);
+//     });
+// });
 
-server.listen(3000, 'localhost');
+// server.listen(3000, 'localhost');
